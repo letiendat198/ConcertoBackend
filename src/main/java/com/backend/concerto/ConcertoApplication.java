a74a3a698f3470b7c3644a0e888100248bbc1fd6
Le Tien Dat <le.tien.dat.198@gmail.com>
2024-08-08 20:49:59 +0700

    First Commit

---
+++ src/main/java/com/backend/concerto/ConcertoApplication.java
@@ -0,0 +1,13 @@
+package com.backend.concerto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/concerto/ConcertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.concerto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
